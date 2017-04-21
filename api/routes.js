0b9d9f20370558dd3876de844805d067f08f33f9
Léonard <robin_monnier@hotmail.com>
2017-04-21 18:08:53 +0200

    mongo db

--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,8 @@
 // app/routes.js
-import fs 			from 'fs'
-import uuid			from 'uuid'
-import path			from 'path'
-import mongoConnect from './config/mongo.js'
 import * as Auth from './Auth.js'
 import * as User from './User.js'
 
-const routes = (apiRoutes, app) => {
+const routes = (apiRoutes) => {
 
 // User authentification  ==========
 apiRoutes.post('/signup', Auth.signupController)
@@ -16,24 +12,12 @@ apiRoutes.post('/signin', Auth.signinController)
 apiRoutes.use(Auth.isLogged)
 apiRoutes.get('/whoami', Auth.whoami)
 
-// User authentification  ==========
-
+// User Datas  ==========
 apiRoutes.get('/profile/:login', User.getInfo)
-
 apiRoutes.post('/profile/:login', User.updateInfo)
-
 apiRoutes.get('/profile/:login/pictures/:id', User.getPicture)
-
 apiRoutes.post('/profile/:login/pictures', User.postPicture)
 
-}
-
-
-// route middleware to make sure a user is logged in
-const isLoggedIn = (req, res, next) => {
-	if (req.isAuthenticated())
-	return next();
-	res.redirect('/');
 }
 
 export default routes
--- api/server.js
+++ api/server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'dist'), {
 }))
 
 const apiRoutes = express.Router()
-routes(apiRoutes, app)
+routes(apiRoutes)
 app.use('/api', apiRoutes)
 
 // launch ======================================================================
